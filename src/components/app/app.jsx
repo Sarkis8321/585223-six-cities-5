49035c2e50ad0d81adb6499d517e44a1e4147d7b
Artser <artserlisa@mail.ru>
2020-09-30 21:26:22 +0300

    3009/2

---
+++ src/components/app/app.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+//import PropTypes from "prop-types";
+
+import {Main} from "../main/main";
+
+
+const App = () => {
+  // eslint-disable-next-line react/prop-types
+
+  return (
+    <Main />
+  );
+};
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-"use strict";
+import React from "react";
+import ReactDOM from "react-dom";
 
+import App from "./components/app/app.jsx";
+
+const offersData = {
+  offersCount: 312
+};
+
+ReactDOM.render(
+    <App offersCount={offersData.offersCount} />,
+    document.getElementById(`root`)
+);
