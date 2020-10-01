07184770079573ef03347bb348042c2bf996a3e8
Artser <artserlisa@mail.ru>
2020-10-01 09:30:04 +0300

    01

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -72,17 +72,17 @@ const Main = (props) => {
             <b className="places__found">{rentOptionsCount} places to stay in Amsterdam</b>
             <form className="places__sorting" action="#" method="get">
               <span className="places__sorting-caption">Sort by</span>
-              <span className="places__sorting-type" tabindex={0}>
+              <span className="places__sorting-type" tabIndex={0}>
                 Popular
                   <svg className="places__sorting-arrow" width={7} height={4}>
                   <use xlinkHref="#icon-arrow-select"></use>
                 </svg>
               </span>
               <ul className="places__options places__options--custom places__options--opened">
-                <li className="places__option places__option--active" tabindex="0">Popular</li>
-                <li className="places__option" tabindex={0}>Price: low to high</li>
-                <li className="places__option" tabindex={0}>Price: high to low</li>
-                <li className="places__option" tabindex={0}>Top rated first</li>
+                <li className="places__option places__option--active" tabIndex="0">Popular</li>
+                <li className="places__option" tabIndex={0}>Price: low to high</li>
+                <li className="places__option" tabIndex={0}>Price: high to low</li>
+                <li className="places__option" tabIndex={0}>Top rated first</li>
               </ul>
 
             </form>
@@ -110,7 +110,7 @@ const Main = (props) => {
                     </button>
                   </div>
                   <div className="place-card__rating rating">
-                    <div class="place-card__stars rating__stars">
+                    <div className="place-card__stars rating__stars">
                       <span style={{width: `80%`}}></span>
                       <span className="visually-hidden">Rating</span>
                     </div>
