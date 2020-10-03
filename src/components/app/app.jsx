a710558ba228df8ab2fb5874fb75735d12354704
Artser <artserlisa@mail.ru>
2020-10-03 20:58:46 +0300

    0310

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import Main from "../main/main";
+import Login from "../login/login";
+import Favorites from "../favorites/favorites";
+import Offer from "../offer/offer";
 
 const App = (props) => {
   const {rentOptionsCount} = props;
 
   return (
-    <Main rentOptionsCount={rentOptionsCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main rentOptionsCount={rentOptionsCount} />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+        <Route path="/offer/:id?" exact component={Offer} />
+
+      </Switch>
+    </BrowserRouter>
   );
 };
 
