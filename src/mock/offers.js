91eca8cd4df4b5e5cfe60fbd1fcc11f69b017a53
Artser <artserlisa@mail.ru>
2020-10-06 17:48:47 +0300

    0610

---
+++ src/mock/offers.js
@@ -0,0 +1,48 @@
+const offers = [
+  {
+    id: 1,
+    title: `Beautiful & luxurious apartment at graet location`,
+    imgLink: `img/apartment-01.jpg`,
+    price: 120,
+    type: `Apartment`,
+    isPremium: true,
+    rating: 80,
+    coord: [52.3909553943508, 4.85309666406198],
+    city: `Amsterdam`
+  },
+  {
+    id: 2,
+    title: `Wood and stone place`,
+    imgLink: `img/room.jpg`,
+    price: 80,
+    type: `Private room`,
+    isPremium: false,
+    rating: 80,
+    coord: [53.556, 9.963],
+    city: `Hamburg`
+  },
+  {
+    id: 3,
+    title: `You can't live closer to Eiffel Tower`,
+    imgLink: `img/apartment-02.jpg`,
+    price: 132,
+    type: `Apartment`,
+    isPremium: false,
+    rating: 80,
+    coord: [48.856626, 2.292919],
+    city: `Paris`
+  },
+  {
+    id: 4,
+    title: `Nice, cozy, warm big bed apartment`,
+    imgLink: `img/apartment-03.jpg`,
+    price: 180,
+    type: `Apartment`,
+    isPremium: true,
+    rating: 100,
+    coord: [50.99, 6.93],
+    city: `Cologne`
+  }
+];
+
+export default offers;
